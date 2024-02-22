a30231f1b927677df1db9c1d5806c29a08316fe1
Daniel Alejandro Flores Sepulveda <160781340+IngIktan@users.noreply.github.com>
2024-02-22 10:22:50 -0600

    Listas
    Metodos para agregar a la lista

---
+++ _007_Listas_AgregarALaLista.py
@@ -0,0 +1,11 @@
+# Daniel Alejandro Flores Sepulveda 21310203
+# Esta práctica se utilizaran metodos de añadir elementos a la lista
+
+s = ["Spider-Man", "Iron Man", "Wonder Woman", "Batman", "Superman"]  # Crear una lista de superhéroes
+print("Lista de Superhéroes: \n", s)#Se imprime la lista de superheroes
+
+s.append('Capitan America') #se agrega el elemento de capitan america al arreglo 
+print("Se añade Capitan America: \n", s) #se imprime el valor de capitan america al arreglo 
+
+s.insert(0, 'Hulk') #se inserta el valor hulk a la primera posicion o la posicion 0 del arreglo 
+print("Se añade Hulk a la primera posicion: \n", s)#se imprime el valor de la posicion 0 del arreglo 
