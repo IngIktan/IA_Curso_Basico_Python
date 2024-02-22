91497ec9fab9d9f50d45c3a544ffe1ef1faa597f
IngIktan <160781340+IngIktan@users.noreply.github.com>
2024-02-21 23:32:10 -0600

    Variables y Strings

---
+++ _001_Variables_Concatenar.py
@@ -0,0 +1,7 @@
+#Daniel Alejandro Flores Sepulveda 21310203 
+#En esta practica vamos a crear una variable y asignaerle una palabra para despues Concatenar dichas variables  
+
+a='vamos a hacer' #usamos una variable para guardar una palabra 
+b= 'el hola mundo'#usamos otra variable para guardar otra palabra cualquiera 
+c=a+b #usamos la variable c para sumar las dos palabras, es decir unir ambas frases 
+print (c) #imprimimos para ver la palabra 
