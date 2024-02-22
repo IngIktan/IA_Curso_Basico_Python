cfabbc3c2b72042f262be77c9634d52f1885e850
IngIktan <160781340+IngIktan@users.noreply.github.com>
2024-02-21 23:35:40 -0600

    Listas

---
+++ _005_Listas_CrearyusarListas.py
@@ -0,0 +1,10 @@
+# Daniel Alejandro Flores Sepulveda 21310203
+# Este programa demuestra cómo crear listas de superhéroes y realizar operaciones básicas con ellas como imprimir la lista o una posicion de ella incluyendo posiciones negativas.
+superheroes = ["Spider-Man", "Iron Man", "Wonder Woman", "Batman", "Superman"]  # Crear una lista de superhéroes
+# Imprimir la lista de superhéroes
+print("Lista de Superhéroes:", superheroes)#Se imprime la lista de superheroes
+primer_superheroe = superheroes[0]  # Acceder a un superhéroe específico por su índice
+print("\nPrimer superhéroe:", primer_superheroe)
+tercer_superheroe = superheroes[2]  # Acceder a un superhéroe específico por su índice
+print("\nTercero superheroina:", tercer_superheroe)
+print("\nSe da la posicion -1 de la lista:",superheroes[-1])#Con este elemento se accede al ultimo valor registrado en este caso es Superman
