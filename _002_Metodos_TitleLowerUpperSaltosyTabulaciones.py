9aa460adfd84dd18eb1de74fcf107d86fd7a2030
IngIktan <160781340+IngIktan@users.noreply.github.com>
2024-02-21 23:33:55 -0600

    Strings y Metodos

---
+++ _002_Metodos_TitleLowerUpperSaltosyTabulaciones.py
@@ -0,0 +1,12 @@
+#Daniel Alejandro Flores Sepulveda 21310203 
+# mEtodo Title lowe y UPper se usa el salto de linea y la tabulacion
+
+x = 'VAMOS A USAR DIFERENTES METODOS CON ESTA PALABRA '
+#en el siguiente codigo se usan tabulaciones y saltos de linea 
+print('usamos Title \n\t', x.title()) #Con el metodo Title se ponen las primeras letras de cada palabra en Mayusculas 
+print('usamos lower\n\t', x.lower()) #Con el metodo lower se ponen todas las letras en minusculas 
+print('usamos UPPER\n\t', x.upper())#Con el metodoo upper se ponen todas las letras en mayusculas
+
+print ('Notas la difenrencia con la original?\n',x) #Se pone un mensaje y se compara con los modificados
+
+
